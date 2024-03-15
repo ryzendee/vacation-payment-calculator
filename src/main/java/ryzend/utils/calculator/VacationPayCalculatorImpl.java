4455d2e5a52bbea669f96535d470b5607855fcad
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-15 13:18:19 +0400

    refator: Refactored calculator implementation

--- src/main/java/ryzend/utils/calculator/VacationPayCalculatorImpl.java
+++ src/main/java/ryzend/utils/calculator/VacationPayCalculatorImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class VacationPayCalculatorImpl implements VacationPayCalculator {
 
-
     @Override
     public double calculateVacPay(double avgSalary, int vacationDays) {
         return avgSalary * vacationDays;

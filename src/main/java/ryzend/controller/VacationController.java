15a2463a722e99df6d8c6bab08c88f3645ea1c50
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-15 10:42:22 +0400

    feat: Implemented vacation controller

--- src/main/java/ryzend/controller/VacationController.java
+++ src/main/java/ryzend/controller/VacationController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ryzend.dto.request.VacationRequest;
+import ryzend.dto.response.VacationResponse;
+import ryzend.service.VacationService;
 
 import javax.validation.Valid;
 
